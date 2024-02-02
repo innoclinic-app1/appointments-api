699e209beec2717ef1d356344553a938a507d74f
Andrew Shumel <155365407+capybo@users.noreply.github.com>
2024-02-02 12:28:25 +0300

    Added basic entities

---
+++ src/Domain/Entities/Appointment.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public int PatientId { get; set; }
+    public int DoctorId { get; set; }
+    public int ServiceId { get; set; }
+    public DateTime DateTime { get; set; }
+    public bool IsApproved { get; set; }
+    
+    public Result? Result { get; set; }
+}
---
+++ src/Domain/Entities/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public class Result
+{
+    public int Id { get; set; }
+    public int AppointmentId { get; set; }
+    public string Complaints { get; set; } = null!;
+    public string Conclusion { get; set; } = null!;
+    public string Recommendations { get; set; } = null!;
+    
+    public Appointment Appointment { get; set; } = null!;
+}
